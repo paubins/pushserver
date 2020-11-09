fe5d4fc9c06631406ccf17cabe7a0b4351a64a5b
Patrick Aubin <p_aubin@apple.com>
2020-11-09 15:19:56 -0800

    made it in django instead of bottle

--- pushserver/pushserver/settings.py
+++ pushserver/pushserver/settings.py
@@ -133,3 +133,5 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
+
+STATIC_ROOT = 'static'
