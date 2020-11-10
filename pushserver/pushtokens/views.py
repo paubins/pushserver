ea4bb54a9c964c4494ecc3195f89e7b65e0a97df
Patrick Aubin <p_aubin@apple.com>
2020-11-09 21:28:24 -0800

    made it in django instead of bottle

--- pushserver/pushtokens/views.py
+++ pushserver/pushtokens/views.py
@@ -20,36 +20,6 @@ class PublishStreamToken(View):
             push_message.delay(push_message_obj.pk, user_token.device_token, custom={"streamToken" : stream_token})
         return HttpResponse('ok')
 
-class StoreRetrieveDeviceToken(View):
-    def post(self, request, *args, **kwargs):
-        user_id = request.POST['userID']
-        device_token = request.POST['deviceToken']
-
-        user_token = Token.objects.filter(user_id=user_id)
-        if len(user_token) == 1:
-            user_token = user_token[0]
-            if user_token.device_token != device_token:
-                user_token.device_token = device_token
-                user_token.save()
-        else:
-            user_token = Token(user_id=user_id, device_token=device_token)
-            user_token.save()
-        return HttpResponse('ok')
-
-
-class StoreRetrieveStreamToken(View):
-    def post(self, request, *args, **kwargs):
-        user_id = request.POST['userID']
-        stream_token = request.POST['streamToken']
-
-        user_token = Token.objects.filter(user_id=user_id)
-        if len(user_token) == 1:
-            user_token = user_token[0]
-            user_token.stream_token = stream_token
-            user_token.save()
-        return HttpResponse('ok')
-
-
 class ResetStreamToken(View):
     def post(self, request, *args, **kwargs):
         stream_token = request.POST['name']
@@ -64,11 +34,41 @@ class ResetStreamToken(View):
             push_message.delay(push_message_obj.pk, user_token.device_token)
         return HttpResponse('ok')
 
+class StoreRetrieveDeviceToken(View):
+    def post(self, request, *args, **kwargs):
+        received_json_data = json.loads(request.body)
+        user_id = received_json_data['userID']
+        device_token = received_json_data['deviceToken']
+
+        user_token = Token.objects.filter(user_id=user_id)
+        if len(user_token) == 1:
+            user_token = user_token[0]
+            if user_token.device_token != device_token:
+                user_token.device_token = device_token
+                user_token.save()
+        else:
+            user_token = Token(user_id=user_id, device_token=device_token)
+            user_token.save()
+        return HttpResponse('ok')
+
+class StoreRetrieveStreamToken(View):
+    def post(self, request, *args, **kwargs):
+        received_json_data = json.loads(request.body)
+        user_id = received_json_data['userID']
+        stream_token = received_json_data['streamToken']
+
+        user_token = Token.objects.filter(user_id=user_id)
+        if len(user_token) == 1:
+            user_token = user_token[0]
+            user_token.stream_token = stream_token
+            user_token.save()
+        return HttpResponse('ok')
 
 class CheckStreamStatus(View):
     def post(self, request, *args, **kwargs):
-        user_id = request.POST['userID']
-        device_token = request.POST['deviceToken']
+        received_json_data = json.loads(request.body)
+        user_id = received_json_data['userID']
+        device_token = received_json_data['deviceToken']
         is_streaming = False
 
         user_token = Token.objects.get(user_id=user_id)
